55d9924f6196d7d7a237ed6cec2a56363c8feb98
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-08-14 23:56:15 -0300

    Incluindo arquivos de configuração

---
+++ demo/src/test/java/sp/gov/fatec/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.gov.fatec.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
