43c7a0ed388fd0ab978a20681e960b7da1a47856
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-08-19 01:48:15 +0000

    Configurando o BD

--- DDL.sql
+++ DDL.sql
@@ -1,30 +1,29 @@
-create schema anotacao;
+create schema cookbook;
 
-use anotacao;
+use cookbook;
 
 create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on anotacao.* to user@'localhost';
+grant select, insert, delete, update on cookbook.* to user@'localhost';
 
-create table usr_usuario (
-  usr_id bigint unsigned not null auto_increment,
-  usr_nome varchar(20) not null,
-  usr_senha varchar(50) not null,
-  primary key (usr_id),
-  unique key uni_usuario_nome (usr_nome)
+create table rec_receita (
+  rec_id bigint unsigned not null auto_increment,
+  rec_nome varchar(20) not null,
+  primary key (rec_id),
+  unique key uni_receita_nome (rec_nome)
 );
 
-create table aut_autorizacao (
-  aut_id bigint unsigned not null auto_increment,
-  aut_nome varchar(20) not null,
-  primary key (aut_id),
-  unique key uni_aut_nome (aut_nome)
+create table ing_ingrediente (
+  ing_id bigint unsigned not null auto_increment,
+  ing_nome varchar(20) not null,
+  primary key (ing_id),
+  unique key uni_ingrediente_nome (ing_nome)
 );
 
-create table uau_usuario_autorizacao (
-  usr_id bigint unsigned not null,
-  aut_id bigint unsigned not null,
-  primary key (usr_id, aut_id),
-  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
-  foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+create table rei_receita_ingrediente (
+  rec_id bigint unsigned not null,
+  ing_id bigint unsigned not null,
+  primary key (rec_id, ing_id),
+  foreign key receita_fk (rec_id) references rec_receita (rec_id) on delete restrict on update cascade,
+  foreign key ingrediente_fk (ing_id) references ing_ingrediente (ing_id) on delete restrict on update cascade
 );
